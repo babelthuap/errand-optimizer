30658853c37a11fb6000b0fe60b15c236674ec49
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-12-03 20:05:03 -0800

    Generate all pairs of nodes

--- js/main.js
+++ js/main.js
@@ -2,41 +2,20 @@ var map;
 
 $(document).ready(function() {
 
-  console.log('jQuery working')
+  var NUM_DESTINATIONS = $('.destination').length;
+
+  // FOR TESTING PURPOSES
+  var HOME = '46684 Windmill Dr';
   
   var geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
   var apiKey = 'AIzaSyAkiQd6LbsShOKAG8vrR-YMF9dn3BQE9oE';
 
-  $('#go').click(centerOnAddress);
-  $('#address').on('keypress', function(e) {
-    if (e.keyCode === 13) centerOnAddress();
-  })
 
-  $('#CALC').click(calcTime);
-
-  function calcTime() {
-    var directionsService = new google.maps.DirectionsService();
-
-    var request = {
-      origin: $('#loc1').val(),
-      destination: $('#loc2').val(),
-      travelMode: google.maps.TravelMode.DRIVING
-    };
-
-    directionsService.route(request, function(data, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        console.log('data:', data);
-        $('#time').text( data.routes[0].legs[0].duration.value );
-      }
-    });
-  }
-
-
-  
-
+  $('.center').click(centerOnAddress);
+  $('#findOptimal').click(findOptimal);
 
   function centerOnAddress() {
-    var address = $('#address').val().replace(/\s+/g, '+');
+    var address = $(this).siblings('.address').val().replace(/\s+/g, '+');
     $.get(geocodeUrl + 'key=' + apiKey + '&address=' + address)
       .done(function(data) {
         if (data.results.length === 0) return; // if it found no matches
@@ -52,29 +31,99 @@ $(document).ready(function() {
       })
   }
 
+  // THIS IS A BRUTE FORCE SOLUTION OF THE TRAVELING SALESMAN PROBLEM
+  function findOptimal() {
+    console.log('findOptimal');
+
+    // create array of the destinations
+    var dests = [];
+    for (var i = 0; i < NUM_DESTINATIONS; i++) {
+      var loc = $('#loc' + i).val();
+      if (loc) { // don't bother with empty destinations
+        dests.push( loc );
+      }
+    }
+
+    // we won't calculate an optimal route with 0 or 1 destinations 
+    if (dests.length < 2) return;
+
+    console.log('dests:', dests);
+
+    // create array of all pairs of destinations (order matters)
+    var pairs = [];
+    for (var i = 0; i < dests.length - 1; i++) {
+      for (var j = i + 1; j < dests.length; j++) {
+        pairs.push([ dests[i], dests[j] ]);
+        pairs.push([ dests[j], dests[i] ]);
+      }
+    }
+
+    console.log('pairs:', pairs)
+
+
+
+    // calcTimeBetween($('#loc1').val(), $('#loc2').val());
+  }
+
+
+  // calculate time to travel between two locations
+  function calcTimeBetween(loc1, loc2) {
+    var directionsService = new google.maps.DirectionsService();
+
+    var request = {
+      origin: loc1,
+      destination: loc2,
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+
+    directionsService.route(request, function(data, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        var time = data.routes[0].legs[0].duration.value;
+        console.log('time between', loc1, 'and', loc2, '=', time);
+      }
+    });
+  }
+
 })
 
 function initMap() {
+  // CENTER ON USER'S LOCATION:
+  // navigator.geolocation.getCurrentPosition(function(pos) {
+  //   var crd = pos.coords;
+  //   map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {
+  //       lat: crd.latitude,
+  //       lng: crd.longitude
+  //     },
+  //     zoom: 12
+  //   });
+  // });
 }
 
-// DEFAULT:
-// map = new google.maps.Map(document.getElementById('map'), {
-//   center: {
-//     lat: 44.92328498029149,
-//     lng: -93.1558840197085
-//   },
-//   zoom: 12
-// });
 
-// CENTER ON USER'S LOCATION:
-// navigator.geolocation.getCurrentPosition(function(pos) {
-//   console.log('position:', pos);
-//   var crd = pos.coords;
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {
-//       lat: crd.latitude,
-//       lng: crd.longitude
-//     },
-//     zoom: 10
-//   });
-// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
